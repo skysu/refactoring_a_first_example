e3699433dfb96fb83b7eca14eb21e673a78a5ba0
user <skysu.code@gmail.com>
2019-07-13 14:23:02 +0100

    Add htmlStatement and test

--- index.mjs
+++ index.mjs
@@ -1,6 +1,6 @@
 'use strict';
 import fs from 'fs';
-import statement from './statement';
+import { statement, htmlStatement } from './statement';
 
 const invoices = JSON.parse(fs.readFileSync('./invoices.json'));
 const plays = JSON.parse(fs.readFileSync('./plays.json'));
@@ -8,9 +8,46 @@ const plays = JSON.parse(fs.readFileSync('./plays.json'));
 const testValue = statement(invoices[0], plays);
 const expectedValue = `Statement for BigCo\n Hamlet: $650.00 (55 seats)\n As You Like It: $475.00 (35 seats)\n Othello: $500.00 (40 seats)\nAmount owed is $1,625.00\nYou earned 47 credits\n`;
 
-console.log(testValue);
+logTest(expectedValue, testValue);
 
-const FgRed = '\x1b[31m';
-const FgGreen = '\x1b[32m';
+const testHTMLValue = htmlStatement(invoices[0], plays);
+const expectedHTMLValue = `\
+<h1>Statement for BigCo</h1>
+<table>
+  <tr>
+    <th>play</th>
+    <th>seats</th>
+    <th>cost</th>
+  </tr>
+  <tr>
+    <td>Hamlet</td>
+    <td>55</td>
+    <td>$650.00</td>
+  </td>
+  <tr>
+    <td>As You Like It</td>
+    <td>35</td>
+    <td>$475.00</td>
+  </td>
+  <tr>
+    <td>Othello</td>
+    <td>40</td>
+    <td>$500.00</td>
+  </td>
+</table>
+<p>Amount owed is <em>$1,625.00</em></p>
+<p>You earned <em>47</em> credits</p>`;
 
-(expectedValue === testValue) ? console.log(FgGreen, 'TEST PASS') : console.log(FgRed, 'TEST FAIL');
+logTest(expectedHTMLValue, testHTMLValue);
+
+function logTest(expectedValue, testValue) {
+  const FgGreen = '\x1b[32m';
+  const FgRed = '\x1b[31m';
+  const Reset = '\x1b[0m'
+  const passColor = FgGreen + '%s' + Reset;
+  const failColor = FgRed + '%s' + Reset;
+
+  console.log('-----------------------------------');
+  (expectedValue === testValue) ? console.log(passColor, 'TEST PASS') : console.log(failColor, 'TEST FAIL');
+  console.log(testValue);
+}
--- statement.mjs
+++ statement.mjs
@@ -14,6 +14,34 @@ function renderPlainText(data) {
   return result;
 }
 
+function htmlStatement(invoice, plays) {
+  return renderHTML(createStatementData(invoice, plays), plays);
+}
+
+function renderHTML(data) {
+  let result = `\
+<h1>Statement for ${data.customer}</h1>
+<table>
+  <tr>
+    <th>play</th>
+    <th>seats</th>
+    <th>cost</th>
+  </tr>\n`;
+  for (let perf of data.performances) {
+    result += `\
+  <tr>
+    <td>${perf.play.name}</td>
+    <td>${perf.audience}</td>
+    <td>${usd(perf.amount)}</td>
+  </td>\n`;
+  }
+  result += `\
+</table>
+<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>
+<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>`;
+  return result;
+}
+
 function usd(aNumber) {
   return new Intl.NumberFormat('en-US', {
     style: 'currency',
@@ -22,4 +50,4 @@ function usd(aNumber) {
   }).format(aNumber/100);
 }
 
-export default statement;
+export { statement, htmlStatement };
