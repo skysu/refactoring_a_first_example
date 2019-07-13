09a0792f3e2fabb4e75fe89fc0aa564f875526fc
user <skysu.code@gmail.com>
2019-07-13 13:15:52 +0100

    Replace volumeCreditsFor to use data on enriched performance

--- statement.mjs
+++ statement.mjs
@@ -66,15 +66,8 @@ function renderPlainText(data, plays) {
   function totalVolumeCredits() {
     let result = 0;
     for (let perf of data.performances) {
-      result += volumeCreditsFor(perf)
+      result += perf.volumeCredits
     }
-    return result;
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ('comedy' === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
     return result;
   }
 
