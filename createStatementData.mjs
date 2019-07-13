e090cd3b7766c3217026af7ae4ad19045d1a1fdd
user <skysu.code@gmail.com>
2019-07-13 16:07:44 +0100

    Delegate amountFor to use PerformanceCalculator

--- createStatementData.mjs
+++ createStatementData.mjs
@@ -48,24 +48,8 @@ function createStatementData(invoice, plays) {
   }
 
   function amountFor(aPerformance) {
-    let result = 0;
-    switch(aPerformance.play.type) {
-      case 'tragedy':
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case 'comedy':
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        break;
-      default:
-        throw new Error(`unknown type: ${aPerformance.play.type}`);
-    }
-    return result;
+    const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
+    return calculator.amount;
   }
 
   function volumeCreditsFor(aPerformance) {
