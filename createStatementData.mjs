3609537322cd1dfacd6449d9c8b631fa033ff3f0
user <skysu.code@gmail.com>
2019-07-13 16:28:16 +0100

    Override volumeCredits in ComedyCalculator

--- createStatementData.mjs
+++ createStatementData.mjs
@@ -50,10 +50,7 @@ class PerformanceCalculator {
   }
 
   get volumeCredits() {
-    let result = 0;
-    result += Math.max(this.performance.audience - 30, 0);
-    if ('comedy' === this.play.type) result += Math.floor(this.performance.audience / 5);
-    return result;
+    return Math.max(this.performance.audience - 30, 0);
   }
 }
 
@@ -74,5 +71,9 @@ class ComedyCalculator extends PerformanceCalculator {
       result += 10000 + 500 * (this.performance.audience - 20);
     }
     return result;
+  }
+
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5);
   }
 }
