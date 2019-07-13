6609da2227c60bde08f190215d582066e96a0f4b
user <skysu.code@gmail.com>
2019-07-13 11:52:23 +0100

    Add colors to test logging

--- index.mjs
+++ index.mjs
@@ -9,4 +9,8 @@ const testValue = statement(invoices[0], plays);
 const expectedValue = `Statement for BigCo\n Hamlet: $650.00 (55 seats)\n As You Like It: $475.00 (35 seats)\n Othello: $500.00 (40 seats)\nAmount owed is $1,625.00\nYou earned 47 credits\n`;
 
 console.log(testValue);
-console.log('TEST', expectedValue === testValue ? 'PASS' : 'FAIL');
+
+const FgRed = '\x1b[31m';
+const FgGreen = '\x1b[32m';
+
+(expectedValue === testValue) ? console.log(FgGreen, 'TEST PASS') : console.log(FgRed, 'TEST FAIL');
