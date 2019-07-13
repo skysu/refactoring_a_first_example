b0a4d5ce4bb9ad2f6071763a214a207569deeeeb
user <skysu.code@gmail.com>
2019-07-13 13:17:44 +0100

    Add totalAmount to enrichPerformance. Copy totalAmount and add param data

--- statement.mjs
+++ statement.mjs
@@ -2,6 +2,7 @@ function statement(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
   statementData.performances = invoice.performances.map(enrichPerformance);
+  statementData.totalAmount = totalAmount(statementData);
   return renderPlainText(statementData, plays);
 
   function enrichPerformance(aPerformance) {
@@ -41,6 +42,14 @@ function statement(invoice, plays) {
     let result = 0;
     result += Math.max(aPerformance.audience - 30, 0);
     if ('comedy' === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
+    return result;
+  }
+
+  function totalAmount(data) {
+    let result = 0;
+    for (let perf of data.performances) {
+      result += perf.amount;
+    }
     return result;
   }
 
