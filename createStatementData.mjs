72464c105f8c64b5e04d8a62fd19d7dfc3b7f7c8
user <skysu.code@gmail.com>
2019-07-13 16:05:39 +0100

    Add getter amount

--- createStatementData.mjs
+++ createStatementData.mjs
@@ -1,7 +1,28 @@
 class PerformanceCalculator {
   constructor(aPerformance, aPlay) {
-    this.performances = aPerformance;
+    this.performance = aPerformance;
     this.play = aPlay;
+  }
+
+  get amount() {
+    let result = 0;
+    switch(this.play.type) {
+      case 'tragedy':
+        result = 40000;
+        if (this.performance.audience > 30) {
+          result += 1000 * (this.performance.audience - 30);
+        }
+        break;
+      case 'comedy':
+        result = 30000;
+        if (this.performance.audience > 20) {
+          result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        break;
+      default:
+        throw new Error(`unknown type: ${this.play.type}`);
+    }
+    return result;
   }
 }
 
