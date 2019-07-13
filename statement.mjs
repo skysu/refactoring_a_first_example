bc43755de1183f271fd629f79e37caa5126a16ac
user <skysu.code@gmail.com>
2019-07-13 12:41:49 +0100

    Extract function appleSauce (totalAmount)

--- statement.mjs
+++ statement.mjs
@@ -1,15 +1,22 @@
 function statement(invoice, plays) {
-  let totalAmount = 0;
+  let totalAmount = appleSauce();
   let result = `Statement for ${invoice.customer}\n`;
   for (let perf of invoice.performances) {
     
     // print line for this order
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
-    totalAmount += amountFor(perf);
   }
   result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
+
+  function appleSauce() {
+    let totalAmount = 0;
+    for (let perf of invoice.performances) {
+      totalAmount += amountFor(perf);
+    }
+    return totalAmount;
+  }
 
   function totalVolumeCredits() {
     let result = 0;
